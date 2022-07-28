ad358de94b0ec543df0b464b07f1aec0cf863602
Eiranya <34960104+Eiranya@users.noreply.github.com>
2022-07-28 22:18:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,9 +20,6 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git openwrt_kiddin9 https://op.supes.top/packages/x86_64' >>feeds.conf.default
 
-# Add date version
-export DATE_VERSION=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
-sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
 
 # 替换默认主题为 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
